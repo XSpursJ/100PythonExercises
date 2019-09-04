49b33dee9d5c230b525bfe52f54405d4b0ed95d8
unknown <sijia@fl-compid-0693.FLUIDLIFE>
2019-09-04 10:51:15 -0600

    q4 completed

---
+++ answers/q4.py
@@ -0,0 +1,30 @@
+s = input("Enter a comma seperated string:")
+l = []
+
+if(len(s) <= 0):
+    print('Empty input!')
+else:
+    startCommaIndex = 0
+    stopCommaIndex = 0
+    appearance = s.count(',')
+
+    for x in range(appearance + 1):
+        stopCommaIndex = s.find(',', startCommaIndex)
+        if(stopCommaIndex != -1):
+            temp = s[startCommaIndex : stopCommaIndex].strip()
+            startCommaIndex = stopCommaIndex + 1
+        else:
+            temp = s[startCommaIndex : len(s)].strip()
+
+        if(temp != ''):
+            l += [temp]
+    t = tuple(l)
+    print(l)
+    print(t)
+
+# Solution:
+# values=raw_input()
+# l=values.split(",")
+# t=tuple(l)
+# print l
+# print t
